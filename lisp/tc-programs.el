2bdc3fa0611d56e497f9f16c7a5c832cd71e1a88
Ooops_t <ooops_t@163.com>
2023-03-19 16:08:08 +0800

    ts-programs: add LSP client eglot for c and c++ mode
    - the LSP server default is clangd
      must install the clangd for your system
    
    Signed-off-by: Ooops_t <ooops_t@163.com>

--- lisp/tc-programs.el
+++ lisp/tc-programs.el
@@ -6,5 +6,12 @@
   :config
   (add-hook 'prog-mode-hook #'format-all-ensure-formatter))
 
+;;; Client for LSP
+(use-package eglot
+  :ensure t
+  :hook
+  (c-mode . eglot-ensure)
+  (c++-mode . eglot-ensure))
+
 (provide 'tc-programs)
 ;;; end of tc-programs
