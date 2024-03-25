432b6b46a2040f7edc872fee287e4145ea9b6033
ooops_t <ooops_t@163.com>
2024-03-25 09:51:38 +0800

    feat: setting encoding and env to ust8
    Signed-off-by: ooops_t <ooops_t@163.com>

--- lisp/tc-edit.el
+++ lisp/tc-edit.el
@@ -2,6 +2,12 @@
 
 ;;; Code:
 
+;; Encoding and envs
+(prefer-coding-system 'utf-8)
+(setenv "LANG" "en_US.UTF-8")
+(setenv "LC_ALL" "en_US.UTF-8")
+(setenv "LC_CTYPE" "en_US.UTF-8")
+
 ;; Not auto save backup file
 (setq make-backup-files nil)
 
