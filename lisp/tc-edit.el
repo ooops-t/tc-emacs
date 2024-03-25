efd69e4aad68edcf62154c78d4423449fe339043
ooops_t <ooops_t@163.com>
2024-03-25 09:21:39 +0800

    feat: add move line and comment org roam
    Signed-off-by: ooops_t <ooops_t@163.com>

--- lisp/tc-edit.el
+++ lisp/tc-edit.el
@@ -8,5 +8,33 @@
 ;; Refresh/revert file
 (global-auto-revert-mode 1)
 
+;; Move line
+;; Reference: https://www.emacswiki.org/emacs/MoveLine
+(defun tc/move-line (n)
+  "Move the current line up or down by N lines."
+  (interactive "p")
+  (setq col (current-column))
+  (beginning-of-line) (setq start (point))
+  (end-of-line) (forward-char) (setq end (point))
+  (let ((line-text (delete-and-extract-region start end)))
+    (forward-line n)
+    (insert line-text)
+    ;; restore point to original column in moved line
+    (forward-line -1)
+    (forward-char col)))
+
+(defun tc/move-line-up (n)
+  "Move the current line up by N lines."
+  (interactive "p")
+  (tc/move-line (if (null n) -1 (- n))))
+
+(defun tc/move-line-down (n)
+  "Move the current line down by N lines."
+  (interactive "p")
+  (tc/move-line (if (null n) 1 n)))
+
+(global-set-key (kbd "M-<up>") 'tc/move-line-up)
+(global-set-key (kbd "M-<down>") 'tc/move-line-down)
+
 (provide 'tc-edit)
 ;;; tc-edit.el ends there
--- lisp/tc-org.el
+++ lisp/tc-org.el
@@ -1,25 +1,27 @@
 ;;; tc-org.el
 
 ;;; Code:
+(use-package org
+  :straight (:type git :repo "https://git.savannah.gnu.org/cgit/emacs/org-mode.git"))
 
-(use-package org-roam
-  :straight (:host github :repo "org-roam/org-roam"
-             :files (:defaults "extensions/*"))
-  :custom
-  (org-roam-directory (file-truename "~/nfs/work/work-status/"))
-  :bind (("C-c n l" . org-roam-buffer-toggle)
-         ("C-c n f" . org-roam-node-find)
-         ("C-c n g" . org-roam-graph)
-         ("C-c n i" . org-roam-node-insert)
-         ("C-c n c" . org-roam-capture)
-         ;; Dailies
-         ("C-c n j" . org-roam-dailies-capture-today))
-  :config
-  ;; If you're using a vertical completion framework, you might want a more informative completion interface
-  (setq org-roam-node-display-template (concat "${title:*} " (propertize "${tags:10}" 'face 'org-tag)))
-  (org-roam-db-autosync-mode)
-  ;; If using org-roam-protocol
-  (require 'org-roam-protocol))
+;; (use-package org-roam
+;;   :straight (:host github :repo "org-roam/org-roam"
+;;              :files (:defaults "extensions/*"))
+;;   :custom
+;;   (org-roam-directory (file-truename "~/nfs/work/work-status/"))
+;;   :bind (("C-c n l" . org-roam-buffer-toggle)
+;;          ("C-c n f" . org-roam-node-find)
+;;          ("C-c n g" . org-roam-graph)
+;;          ("C-c n i" . org-roam-node-insert)
+;;          ("C-c n c" . org-roam-capture)
+;;          ;; Dailies
+;;          ("C-c n j" . org-roam-dailies-capture-today))
+;;   :config
+;;   ;; If you're using a vertical completion framework, you might want a more informative completion interface
+;;   (setq org-roam-node-display-template (concat "${title:*} " (propertize "${tags:10}" 'face 'org-tag)))
+;;   (org-roam-db-autosync-mode)
+;;   ;; If using org-roam-protocol
+;;   (require 'org-roam-protocol))
 
 (use-package org-bullets
   :straight (:host github :repo "sabof/org-bullets"
