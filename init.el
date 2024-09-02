9e5256ca92d2807907010fb92ded2730a84bf935
m0nk <hac.m0nk@gmail.com>
2024-09-02 10:42:02 +0800

    change fonts
    Signed-off-by: m0nk <hac.m0nk@gmail.com>

--- init.el
+++ init.el
@@ -2,6 +2,17 @@
 
 ;; code
 
+;; Init package
+(package-initialize)
+(setq package-archives '(("gnu" . "https://mirrors.ustc.edu.cn/elpa/gnu/")
+                         ("melpa" . "https://mirrors.ustc.edu.cn/elpa/melpa/")
+                         ("nongnu" . "https://mirrors.ustc.edu.cn/elpa/nongnu/")))
+
+;; User custom file path
+(setq custom-file
+      (concat user-emacs-directory "custom.el"))
+(load custom-file 'noerror)
+
 ;; Disable startup screen and message
 (setq inhibit-startup-screen t)
 (setq inhibit-startup-message t)
@@ -24,7 +35,7 @@
 ;; Set fonts
 (when (display-graphic-p)
   (set-face-attribute 'default nil
-		      :family "PT Mono"
+		      :family "Iosevka Comfy"
 		      :foundry "*"
 		      :width 'normal
 		      :height (* 12 10)
@@ -33,7 +44,19 @@
   ;; Reference: http://xahlee.info/emacs/emacs/emacs_set_font_zh.html
   (set-fontset-font t 'han
 		    (cond
-		     ((member "PT Mono" (font-family-list)) "PT Mono"))))
+		     ((member "PT Mono" (font-family-list)) "PT Mono")))
+  ;; Enable the ligature mode
+  (use-package ligature
+    :ensure t
+    :config
+    ;; Enable all Iosevka ligatures in programming modes
+    (ligature-set-ligatures 'prog-mode '("<---" "<--"  "<<-" "<-" "->" "-->" "--->" "<->" "<-->" "<--->" "<---->" "<!--"
+					 "<==" "<===" "<=" "=>" "=>>" "==>" "===>" ">=" "<=>" "<==>" "<===>" "<====>" "<!---"
+					 "<~~" "<~" "~>" "~~>" "::" ":::" "==" "!=" "===" "!=="
+					 ":=" ":-" ":+" "<*" "<*>" "*>" "<|" "<|>" "|>" "+:" "-:" "=:" "<******>" "++" "+++"))
+    ;; Enables ligature checks globally in all buffers. You can also do it
+    ;; per mode with `ligature-mode'.
+    (global-ligature-mode t)))
 
 ;;; init.el ends here
 
