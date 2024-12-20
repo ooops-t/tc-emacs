2e322aa752a367bc38fad4900077aad4a326f328
Ooops_t <ooops_t@163.com>
2024-12-20 15:09:16 +0800

    Update fonts
    Signed-off-by: Ooops_t <ooops_t@163.com>

--- init.el
+++ init.el
@@ -4,10 +4,13 @@
 
 ;; Init package
 (require 'package)
+;; If you use in Emacs<27, you may want to add something like
+;; Reference: https://elpa.gnu.org/
+(unless (fboundp 'package-activate-all) (package-initialize))
 (package-initialize)
 (setq package-archives '(("gnu" . "https://mirrors.ustc.edu.cn/elpa/gnu/")
-                         ("melpa" . "https://mirrors.ustc.edu.cn/elpa/melpa/")
-                         ("nongnu" . "https://mirrors.ustc.edu.cn/elpa/nongnu/")))
+                         ("melpa" . "https://mirrors.ustc.edu.cn/elpa/melpa/")))
+;; Not use this ("nongnu" . "https://mirrors.ustc.edu.cn/elpa/nongnu/")
 
 ;; User custom file path
 (setq custom-file
@@ -44,8 +47,16 @@
 
 ;; Set fonts
 (when (display-graphic-p)
+  (defcustom tc/fonts-default "Iosevka"
+    "Default Fonts"
+    :type 'string
+    :group 'tc/fonts)
+  (defcustom tc/fonts-chinese "LXGW WenKai Mono"
+    "Chinese fonts"
+    :type 'string
+    :group 'tc/fonts)
   (set-face-attribute 'default nil
-		      :family "Iosevka Comfy"
+		      :family tc/fonts-default
 		      :foundry "*"
 		      :width 'normal
 		      :height (* 12 10)
@@ -54,16 +65,16 @@
   ;; Reference: http://xahlee.info/emacs/emacs/emacs_set_font_zh.html
   (set-fontset-font t 'han
 		    (cond
-		     ((member "PT Mono" (font-family-list)) "PT Mono")))
+		     ((member tc/fonts-chinese (font-family-list)) tc/fonts-chinese)))
   ;; Enable the ligature mode
   (use-package ligature
     :ensure t
     :config
     ;; Enable all Iosevka ligatures in programming modes
     (ligature-set-ligatures 'prog-mode '("<---" "<--"  "<<-" "<-" "->" "-->" "--->" "<->" "<-->" "<--->" "<---->" "<!--"
-					 "<==" "<===" "<=" "=>" "=>>" "==>" "===>" ">=" "<=>" "<==>" "<===>" "<====>" "<!---"
-					 "<~~" "<~" "~>" "~~>" "::" ":::" "==" "!=" "===" "!=="
-					 ":=" ":-" ":+" "<*" "<*>" "*>" "<|" "<|>" "|>" "+:" "-:" "=:" "<******>" "++" "+++"))
+  					 "<==" "<===" "<=" "=>" "=>>" "==>" "===>" ">=" "<=>" "<==>" "<===>" "<====>" "<!---"
+  					 "<~~" "<~" "~>" "~~>" "::" ":::" "==" "!=" "===" "!=="
+  					 ":=" ":-" ":+" "<*" "<*>" "*>" "<|" "<|>" "|>" "+:" "-:" "=:" "<******>" "++" "+++"))
     ;; Enables ligature checks globally in all buffers. You can also do it
     ;; per mode with `ligature-mode'.
     (global-ligature-mode t)))
